e2e51c203e73aacbe5ade02c3bff8eece1ca8486
Justin Beaulieu <beaulieu527@gmail.com>
2017-02-25 21:25:05 -0500

    4.4 added order controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 
 //smooth scrolling scrollspy navigation
  var $root = $('html, body');
-  $('.navbar-nav .navbar-footer a').click(function() {
+  $('.navbar-nav a').click(function() {
   var href = $.attr(this, 'href');
   $root.animate({
     scrollTop: $(href).offset().top
