7f6cdf4ea82b0fc40f00e123d6a395d1fd981d0d
Justin Beaulieu <beaulieu527@gmail.com>
2017-03-05 15:20:14 -0500

    5.4 added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  ddevise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  resources :users
   resources :products
   get 'static_pages/about'
   get 'static_pages/contact'
