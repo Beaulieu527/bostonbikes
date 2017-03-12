419440dc9cca32de5d4030a3afa3306005317b51
Justin Beaulieu <beaulieu527@gmail.com>
2017-03-11 23:35:48 -0500

    5.8 added ratings

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,12 @@ class Product < ApplicationRecord
 	def highest_rating_comment
 		comments.rating_desc.first
 	end
+	
 	def lowest_rating_comment
 		comments.rating_desc.first
 	end	
+
+	def average_rating
+    	comments.average(:rating).to_f
+	end
 end
