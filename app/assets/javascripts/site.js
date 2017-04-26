eb5ee8907f373459226c4829aa9c882d12896fb6
Justin Beaulieu <beaulieu527@gmail.com>
2017-04-26 02:45:17 -0400

    6.9addjquery ady  adds

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,26 @@
-$(document).on('turbolinks:load', function(){
+var refreshRating = function() {
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
     });
-});
+};
 
+$(document).on('turbolinks:load ajaxSuccess', function(){
+       
+       refreshRating();
+       
+      $('.img-zoom').elevateZoom({
+        zoomType: "lens",
+        lensShape : "round",
+        lensSize    : 150  
+    });
+      
+       $('.menu-btn').click(function(){
+        $('.responsive-menu').toggleClass('expand');
+  });
+  //any rails flashes are set to delayed fadeout over 2s
+      $('.alert').delay(3000).fadeOut(2000);
+ }); 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -73,11 +73,6 @@ class ProductsController < ApplicationController
     end
   end
 
-def index
-  byebug
-  @articles = Article.find_recent
-end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
