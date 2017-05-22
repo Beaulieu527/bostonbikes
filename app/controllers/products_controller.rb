a16c5cf152221bb23f73547509655f2b59cd7677
Justin Beaulieu <beaulieu527@gmail.com>
2017-05-22 01:26:56 -0400

    added NewRelic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,7 +21,7 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
-
+    @product.viewed!
   end
 
   # GET /products/new
@@ -83,7 +83,5 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:name, :description, :image_url, :color, :price)
     end
-
-
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,33 @@
 class Product < ApplicationRecord
-	has_many :orders
-	has_many :comments
-	validates :name, presence: true
-	def self.search(search_term)
-  		Product.where("name LIKE ?", "%#{search_term}%")
-	end
+  has_many :orders
+  has_many :comments
+  validates :name, presence: true
+  
+  def self.search(search_term)
+    if Rails.env.development?
+      Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      Product.where("name ilike?", "%#{search_term}%")
+    end 
+  end
 
-	def highest_rating_comment
-		comments.rating_desc.first
-	end
-	
-	def lowest_rating_comment
-		comments.rating_asc.first
-	end	
+   def highest_rating_comment
+    comments.rating_desc.first
+  end
 
-	def average_rating
-    	comments.average(:rating).to_f
-	end
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+
+  def average_rating
+    comments.average(:rating).to_f
+  end
+
+  def views
+    $redis.get("product:#{id}") # this is equivalent to 'GET product:1'
+  end
+
+  def viewed!
+    $redis.incr("product:#{id}") # this is equivalent to 'INC product:1'
+  end
 end
