f86552187589fd919667dcaf81fe17ceb64fc363
Justin Beaulieu <beaulieu527@gmail.com>
2017-06-08 23:58:39 -0400

    fixed comment section

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,38 +1,38 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
+
   before do
     @user1 = FactoryGirl.create(:user)
-    @user2 = FactoryGirl.create(:admin)
+    @user2 = FactoryGirl.create(:user)
   end
 
   describe 'GET #show' do
-
     context 'User is logged in' do
       before do
         sign_in @user1
       end
 
       it 'loads correct user details' do
-        get :show, params: { id: @user1.id }
+        get :show, id: @user1.id
+        expect(response).to be_success
         expect(response).to have_http_status(200)
         expect(assigns(:user)).to eq @user1
       end
 
-      it 'cannot access show page of user2' do
-        get :show, params: { id: @user2.id }
+      it 'denies access to show other users' do
+        get :show, id: @user2.id
         expect(response).to have_http_status(302)
-        expect(assigns(:user)).not_to eq @user1
         expect(response).to redirect_to(root_path)
       end
     end
 
-    context 'no user is logged in' do
-      it 'redirects to sign in' do
-        get :show, params: { id: @user1.id }
-        redirect_to(root_path)
+    context 'No user is logged in' do
+      it 'redirects to login' do
+        get :show, id: @user1.id
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
-
   end
+
 end
